b45f13f0157f00c28317d8e7fbce9ac30ed07ace
johan <johan@DESKTOP-V1ARLMU>
2024-10-08 17:33:12 -0600

    Add project files.

---
+++ JS/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "¿Qué significa la sigla HTML?", 
+    answer: "HyperText Markup Language",
+    
+    options: [
+      "Hyperlink Text Markup Language",
+      "HyperText Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "Home Tool Markup Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Cuál es el propósito del método addEventListener en JavaScript?",
+    answer: "Escuchar eventos como clicks o teclados en un elemento",
+    options: [
+      "Agregar un nuevo elemento HTML al DOM",
+      "Añadir estilos CSS a un elemento",
+      "Crear una nueva función",
+      "Escuchar eventos como clicks o teclados en un elemento"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Qué propiedad de CSS se utiliza para cambiar el color de fondo de un elemento?",
+    answer: "background-color",
+    options: [
+      "background-color",
+      "color",
+      "border-color",
+      "text-color"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Cuál es la forma correcta de declarar una variable en JavaScript?",
+    answer: "let variable = 5;",
+    options: [
+      "variable = 5;",
+      "create variable = 5;",
+      "var = 5;",
+      "let variable = 5;"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Qué valor de la propiedad position en CSS coloca un elemento en relación con el primer contenedor posicionado (que no sea estático)??",
+    answer: "absolute",
+    options: [
+      "absolute",
+      "static",
+      "fixed",
+      "relative"
+    ]
+  },
+  
+];
